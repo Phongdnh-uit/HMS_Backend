11a0ca47f6547fd233203aa86c146a668dca75b5
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2025-11-27 21:27:57 +0700

    🚧 build(server): ci workflow and add test properties

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-validation")
     api("io.github.perplexhub:rsql-jpa-spring-boot-starter:6.0.32")
     api("org.mapstruct:mapstruct:1.6.3")
+    api("com.h2database:h2")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
