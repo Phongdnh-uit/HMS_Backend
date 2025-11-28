253a59317db70461f5cf86b9ef0ef2949a7294bc
Shiroo <hmegxva@gmail.com>
2025-11-28 09:18:12 +0700

    Fix: rename folder medicine service

--- patient-service/src/main/java/com/hms/medicine_service/securities/UserContext.java
+++ patient-service/src/main/java/com/hms/medicine_service/securities/UserContext.java
@@ -6,12 +6,12 @@ import lombok.Setter;
 public class UserContext {
     private static final ThreadLocal<User> currentUser = new ThreadLocal<>();
 
-    public static void setUser(User user) {
-        currentUser.set(user);
-    }
-
     public static User getUser() {
         return currentUser.get();
+    }
+
+    public static void setUser(User user) {
+        currentUser.set(user);
     }
 
     public static void clear() {
