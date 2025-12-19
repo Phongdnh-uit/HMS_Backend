707d8dc909d1efb24874d99ea16e91e6365a2219
chinhbmt122 <chinhkkmmzz@gmail.com>
2025-12-19 17:45:54 +0700

    fix: fix test file

--- appointment-service/src/test/java/com/hms/appointment_service/AppointmentServiceApplicationTests.java
+++ appointment-service/src/test/java/com/hms/appointment_service/AppointmentServiceApplicationTests.java
@@ -1,9 +1,15 @@
-package com.hms.patient_service;
+package com.hms.appointment_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = {
+        // Disable cloud config and eureka for tests
+        "spring.cloud.config.enabled=false",
+        "eureka.client.enabled=false",
+        "spring.cloud.discovery.enabled=false",
+        // Use H2 in-memory database
         "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
         "spring.datasource.driverClassName=org.h2.Driver",
         "spring.datasource.username=sa",
@@ -12,10 +18,12 @@ import org.springframework.boot.test.context.SpringBootTest;
         "spring.jpa.show-sql=true",
         "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
 })
-class PatientServiceApplicationTests {
+@ActiveProfiles("test")
+class AppointmentServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        // Basic test to verify Spring context loads successfully
     }
 
 }
